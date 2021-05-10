49ffb734356560fd44d0c6cefacfcaa1c90315f4
iru0225 <irsyadhafizh2@gmail.com>
2021-05-10 08:54:08 +0700

    add packages & update index page

--- src/pages/base-page/index.pages.jsx
+++ src/pages/base-page/index.pages.jsx
@@ -1,31 +1,80 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.style.scss';
 
-class IndexPage extends React.Component{
+import HeaderComponent from '../../components/header/header.component';
+import RestaurantList from '../../components/restaurant-list/restaurant-list.component';
+import InputComponent from '../../components/input/input.component';
+import SelectComponent from '../../components/select/select.component';
 
-    render(){
-        return(
-            <div className="base-page">
-                <header className="header">
-                    <nav className="header-nav">
-                        <h2>Some Logo</h2>
 
-                        <div className="user">
-                            <div className="order">
-                                Some order
-                            </div>
-                            <div className="user-setting">
-                                User setting
-                            </div>
-                        </div>
-                    </nav>
-                </header>
-                <div className="content">
-                    <h1>web content</h1>
-                </div>
-            </div>
-        )
+
+const IndexPage = () => {
+    const [hour, setHour] = useState([]);
+    const [minute, setMinute] = useState([]);
+    const [ampm, setAmpm] = useState(['AM', 'PM']);
+
+    useEffect(() => {
+        let hours = [];
+        let minutes = [];
+
+        for (let i = 0; i <= 12; i++) {
+            let temp = null;
+            if (i.length < 2) {
+                temp = {value: `0${i}`}
+                return hours.push(temp);
+            }
+
+            temp = {value: `${i}`};
+            hours.push(temp);
+        }
+
+        for (let i = 0; i <= 59; i++) {
+            let temp = null;
+            if (i.length < 2) {
+                temp = {value: `0${i}`}
+                return minutes.push(temp);
+            }
+
+            temp = {value: `${i}`};
+            minutes.push(temp);
+        }
+
+        setHour(hours);
+        setMinute(minutes);
+    }, [])
+    
+
+    const testFunction = (val) => {
+        console.log(val);
     }
+
+    return(
+        <div className="base-page">
+            <HeaderComponent/>
+            <div className="content">
+                <div className="search">
+                    <InputComponent
+                        type={'date'}
+                        parentCallback={testFunction}
+                        labelName={'Booking Date'}
+                    />
+                    <SelectComponent
+                        labelName={'Hour'}
+                        items={hour}
+                    />
+                    <SelectComponent
+                        labelName={'Minute'}
+                        items={minute}
+                    />
+                    <SelectComponent
+                        labelName={'AM/PM'}
+                        items={ampm}
+                    />
+                </div>
+                <RestaurantList/>
+            </div>
+        </div>
+    )
 }
 
 export default IndexPage
