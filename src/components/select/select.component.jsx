7117db8dadc8417a36011f0e14cf8158be0925c3
iru0225 <irsyadhafizh2@gmail.com>
2021-05-10 21:53:46 +0700

    create custom select component

---
+++ src/components/select/select.component.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import  ClickAwayListener from 'react-click-away-listener';
+import './select.style.scss';
+
+const SelectComponent = ({optionList, selectLabel, parentCallback}) => {
+    const [open, setOpen] = useState(false);
+    const [selectData, setSelectData] = useState(null);
+
+    const handleClickAway = () => {
+        setOpen(false);
+    }
+
+    const openClose = () => {
+        let openList = !open;
+        setOpen(openList);
+    }
+
+    const selectedData = async (e) => {
+        let data = {id: e.target.getAttribute('data'), label: e.target.innerText};
+        await setSelectData(data);
+        parentCallback(data)
+        openClose();
+    }
+    
+    return(
+        <ClickAwayListener onClickAway={handleClickAway}>
+            <div className="select-wrapper">
+                <div className="select-group" onClick={openClose}>
+                    <input type="text" readOnly required value={selectData ? selectData.label : ''}/>
+                    <label className={`label ${selectData || open ? 'focus' : null}`}>{selectLabel}</label>
+                    <span className="bar"/>
+                </div>
+                <div className={`select-dropdown ${open ? 'open' : null}`}>
+                    <div className="option-wrapper">
+                        {
+                            optionList.map(e => 
+                                <span className="option-list" key={e.id} data={e.id} onClick={selectedData}>{e.label}</span>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+        </ClickAwayListener>
+    )
+}
+
+export default SelectComponent;
