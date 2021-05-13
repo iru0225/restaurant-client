43d269264a703a4557632ea1992815dced99bbd1
iru0225 <irsyadhafizh2@gmail.com>
2021-05-14 01:09:05 +0700

    add user state & swicth route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import './App.css';
 
-import IndexPage from './pages/base-page/index.pages';
+import HeaderComponent from './components/header/header.component.jsx';
+import IndexPage from './pages/base-page/index.page';
+import SigninPage from './pages/signin/signin.page';
+import OrderPage from './pages/order/order.page';
 
-function App() {
+const CONTENT_STYLES = {
+  marginTop: '5rem'
+}
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!user) {
+      let data = localStorage.getItem('user');
+      if (!data) {
+        return history.push('/signin');
+      }
+
+      setUser(JSON.parse(data));
+    }
+  }, [user]);
+
+  const loginHandle = (val) => {
+    setUser(val);
+  }
+
+  const logoutHandle = (val) => {
+    setUser(null);
+    localStorage.removeItem('user')
+  }
+  
   return (
     <div className="App">
-      <IndexPage/>
+      <HeaderComponent logout={logoutHandle} user={user}/>
+      <div style={CONTENT_STYLES} className="content">
+        <Switch>
+          <Route exact path="/">
+            {
+              !user ? <Redirect to='/signin'/> : <IndexPage user={user}/>
+            }
+          </Route>
+          <Route path="/signin">
+            {
+              user ? <Redirect to='/'/> : <SigninPage loginHandle={loginHandle} user={user}/>
+            }
+          </Route>
+          <Route path="/order_history">
+            {
+              !user ? <Redirect to='/signin'/> : <OrderPage user={user}/>
+            }
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
