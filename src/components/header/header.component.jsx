9128e965d64cfa0ec8f774db9d746144ccb8fe9a
iru0225 <irsyadhafizh2@gmail.com>
2021-05-14 00:59:41 +0700

    add signin & signout button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.style.scss';
-import MenuComponent from '../menu/menu.component';
 
-const HeaderComponent = () => {
+const HeaderComponent = ({logout, user}) => {
+
+    const logoutHandle = (e) => {
+        e.preventDefault();
+        logout(null);
+    }
     return(
         <header id="header">
             <nav id="nav">
                 <div className="header-icon">
                     <p>
-                        <a>Some Logo</a>
+                        <Link to="/">Some Logo</Link>
                     </p>
                 </div>
                 <div className="menu">
-                    <MenuComponent/>
+                    <div className="nav-user-order">
+                        <div className="nav-order">
+                            <Link to="/order_history">Order History</Link>
+                        </div>
+                        <div className="nav-order">
+                            {
+                                user ? <span onClick={logoutHandle}>Signout</span> : <Link to="/signin">Signin</Link>
+                            }
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
