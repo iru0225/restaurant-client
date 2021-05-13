76a2e0f5be1b6e9bfd5c5c269107621b34f639da
iru0225 <irsyadhafizh2@gmail.com>
2021-05-14 01:02:22 +0700

    updata modal component

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import  ClickAwayListener from 'react-click-away-listener';
 import './modal.style.scss';
 
 const Modal = ({open, onClose, children}) => {
@@ -9,14 +8,12 @@ const Modal = ({open, onClose, children}) => {
     return ReactDom.createPortal(
         <>
             <div className="modal-overlay"/>
-            <ClickAwayListener onClickAway={onClose}>
                 <div className="modal-content">
                     <button className="close-modal" onClick={onClose}>&times;</button>
                     <div className="modal-body">
                         {children}
                     </div>
                 </div>
-            </ClickAwayListener>
         </>, document.getElementById('portal')
     )
 }
