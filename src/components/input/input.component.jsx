6598cd5f2d767e1643d1c4bc7357775860a57d27
iru0225 <irsyadhafizh2@gmail.com>
2021-05-14 01:01:01 +0700

    add logic if befor today is disable in dat type input

--- src/components/input/input.component.jsx
+++ src/components/input/input.component.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
+import moment from 'moment';
 import './input.style.scss';
 
-const InputComponent = ({groupClass, type, maxLength, parentCallback, labelName}) => {
+const InputComponent = ({groupClass, type, maxLength, parentCallback, labelName, error, beforeToday}) => {
     const [currentValue, setCurrentValue] = useState(null);
     // const [oldValue, setOldValue] = useState(null);
 
@@ -35,11 +36,27 @@ const InputComponent = ({groupClass, type, maxLength, parentCallback, labelName}
     
     return(
         <div className={`group  ${groupClass}`}>
-            <div className="form-group">
-                <input type={`${type === 'number' ? 'text' : type}`} className={type} onInput={inputFunction} required/>
-                <span className="bar"></span>
+            <div className={`form-group ${error.hasError ? 'error' : null}`}>
+                {
+                    type === 'date'
+                    ?
+                        !beforeToday
+                        ?
+                            <input type={type} className={type} onInput={inputFunction} required/>
+                        :
+                            <input type={type} min={moment().format('YYYY-MM-DD')} className={type} onInput={inputFunction} required/>
+                    :
+                        <input type={`${type === 'number' ? 'text' : type}`} className={type} onInput={inputFunction} required/>
+                }
+                <span className={`bar ${error.hasError ? 'error' : null}`}></span>
                 <label>{labelName}</label>
             </div>
+            {
+                error.hasError ?
+                    <span style={{color: '#DA3D26'}}>{error.message}</span>
+                :
+                    null
+            }
         </div>
     )
 }
